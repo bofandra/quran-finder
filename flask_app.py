85b1f5d3e63782d221ec4cca8bcaa8a7c49a6a6a
bofandra <bofandra@gmail.com>
2024-06-19 15:06:31 +0700

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,45 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request
+from flask_jsonpify import jsonpify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/find')
+def find():
+    # transform query from user
+    q = request.args.get('q')
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer('use-cmlm-multilingual')
+    encoded_query_text = model.encode(q)
+    print(q)
+
+    # get encoded quran text
+    file = open('encoded_quran_cmlm.sav','rb')
+    encoded_quran_text = pickle.load(file)
+    file.close()
+
+    # compare query to each quran verse
+    i = 0
+    text_similarity = []
+    for encoded_quran_ayat in encoded_quran_text:
+      similarity = encoded_query_text @ encoded_quran_ayat.T
+      text_similarity.append(similarity)
+      i=i+1
+      print(i)
+
+    # insert the similarity value to dataframe & sort it
+    quran = pd.read_csv('quran-simple-clean.txt', delimiter="|")
+    quran['similarity'] = text_similarity
+    sorted_quran = quran.sort_values(by='similarity', ascending=False)
+    df_list = sorted_quran.values.tolist()
+    JSONP_data = jsonpify(df_list)
+    return JSONP_data
+
